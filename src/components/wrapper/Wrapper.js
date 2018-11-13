cbb0a16bba7ccdab9f2c64c35c32a9f0a725c127
Akallabet <grazianos886@gmail.com>
2018-11-13 23:04:50 +0000

    improve .htaccess

--- src/components/wrapper/Wrapper.js
+++ src/components/wrapper/Wrapper.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
-import { Redirect, withRouter } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 
 import { routesIds } from '../routes/routes-list'
 
